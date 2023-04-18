544571661a7f1a161cbc715ac96802256c12522a
Raiane <raiane.oliveira404@gmail.com>
2023-04-18 18:06:47 -0300

    feat: query through filter button form

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,52 @@
 import "./Search.css";
 import { FunnelSimple, MagnifyingGlass } from "@phosphor-icons/react";
 import { useState } from "react";
+import data from "../data";
 
 export function Search({ setSearchQuery }) {
   const [inputSearch, setInputSearch] = useState("");
+  const [isFilterBtnActive, setIsFilterBtnActive] = useState(false);
+
+  const newData = { ...data };
+  let tags = new Set();
+  Object.values(newData).map((column) =>
+    column.map((task) => {
+      task.tags.map((tag) => tags.add(tag));
+    })
+  );
 
   function handleChange(e) {
-    let querySearch = e.target.value;
+    const querySearch =
+      e.type === "change" ? e.target.value : e.target.textContent;
+
     setInputSearch(querySearch);
     setSearchQuery(querySearch.trim() ? querySearch : "");
+    if (e.type !== "change") setIsFilterBtnActive(false);
   }
 
+  function handleFilterByTags(e) {
+    setIsFilterBtnActive(!isFilterBtnActive);
+  }
+
+  let countKeySelect = 0;
   return (
     <form onSubmit={(e) => e.preventDefault()} className="search-form">
-      <button type="button" className="search-btn">
+      <button type="button" onClick={handleFilterByTags} className="search-btn">
         <FunnelSimple />
         <span>Filtrar</span>
       </button>
+
+      {isFilterBtnActive && (
+        <div className="select-search-form">
+          <ul>
+            {Array.from(tags).map((tag) => (
+              <li key={`${tag}${countKeySelect++}`} onClick={handleChange}>
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       <div className="search-bar">
         <MagnifyingGlass />
