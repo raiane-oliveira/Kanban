18b78e9b93790ee87129c7315b1120ea1bb65103
Raiane <raiane.oliveira404@gmail.com>
2023-04-18 18:40:23 -0300

    feat: edit name of kanban

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,34 @@
 import { PencilSimple } from "@phosphor-icons/react";
 
 import "./Header.css";
+import { useState } from "react";
 
 export function Header({ title, img, imgAlt }) {
+  const [currentTitle, setCurrentTitle] = useState(title);
+  const [isEditingTitle, setIsEditingTitle] = useState(false);
+
+  function handleEditTitle() {
+    setIsEditingTitle(!isEditingTitle);
+  }
+
   return (
     <header className="header">
       <div className="title-header">
-        <h1>{title}</h1>
-        <PencilSimple weight="fill" />
+        {isEditingTitle ? (
+          <form onSubmit={handleEditTitle}>
+            <input
+              type="text"
+              value={currentTitle}
+              className="input-edit-header"
+              onBlur={handleEditTitle}
+              onChange={(e) => setCurrentTitle(e.target.value)}
+              autoFocus
+            />
+          </form>
+        ) : (
+          <h1>{currentTitle}</h1>
+        )}
+        <PencilSimple onClick={handleEditTitle} weight="fill" />
       </div>
       <img src="" alt="" className="img-profile" />
     </header>
