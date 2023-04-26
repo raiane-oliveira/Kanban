d965e1948cb62217a1c182c069e369c75dc4de74
Raiane <raiane.oliveira404@gmail.com>
2023-04-26 13:56:15 -0300

    feat: responsive menu burger

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,33 +8,49 @@ import {
   List,
   Users,
 } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
 
 export function Sidebar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleMenuToggle() {
+    setShowMenu(!showMenu);
+  }
+
+  useEffect(() => {
+    const contentContainer = document.querySelector(".content-container");
+    contentContainer.addEventListener("click", () => setShowMenu(false));
+  }, [showMenu]);
+
   return (
-    <aside className="sidebar">
+    <aside className={!showMenu ? `sidebar` : `sidebar active-menu`}>
       <div className="menu-wrapper">
-        <List className="menu" />
+        <button onClick={handleMenuToggle} type="button" className="menu-btn">
+          <List className="menu-img" />
+        </button>
       </div>
 
-      <img src={logo} alt="Logo em espiral Kanban" className="logo" />
-      <nav className="sidebar-navigation">
-        <NavLink to="/">
-          <DeviceTabletSpeaker weight="fill" />
-          <span>Boards</span>
-        </NavLink>
-        <a href="#">
-          <Users weight="fill" />
-          <span>Equipes</span>
-        </a>
-        <a href="#">
-          <FileText weight="fill" />
-          <span>Relatórios</span>
-        </a>
-        <NavLink to="/settings">
-          <Gear />
-          <span>Ajustes</span>
-        </NavLink>
-      </nav>
+      <div className="sidebar-content-wrapper">
+        <img src={logo} alt="Logo em espiral Kanban" className="logo" />
+        <nav className="sidebar-navigation">
+          <NavLink onClick={handleMenuToggle} to="/">
+            <DeviceTabletSpeaker weight="fill" />
+            <span>Boards</span>
+          </NavLink>
+          <a onClick={handleMenuToggle} href="#">
+            <Users weight="fill" />
+            <span>Equipes</span>
+          </a>
+          <a onClick={handleMenuToggle} href="#">
+            <FileText weight="fill" />
+            <span>Relatórios</span>
+          </a>
+          <NavLink onClick={handleMenuToggle} to="/settings">
+            <Gear />
+            <span>Ajustes</span>
+          </NavLink>
+        </nav>
+      </div>
     </aside>
   );
 }
